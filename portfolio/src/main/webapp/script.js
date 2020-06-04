c8d8068228907c87d53964f39794290837b3fc7a
Nicholas Husin <nicholashusin@gmail.com>
2020-06-04 19:10:17 +0000

    Add client-side logic to handle authentication and display appropriate UI according to user privilege level
    - User can now login to the site with their email
    - When adding comments, users' ldap will now be used as their username
    - Only people with @google emails can use the comment section

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -14,28 +14,111 @@
 
 const YOUTUBE_EMBED_URL         = 'https://www.youtube.com/embed';
 const COMMENT_FETCH_URL         = '/comment-data';
-const COMMENT_SECTION_ID        = 'home-comments';
+const COMMENT_SECTION_ID        = 'comments-list';
 const COMMENT_TEXT_ID           = 'comment-text';
+const COMMENT_LDAP_ID           = 'ldap';
 const COMMENT_SECTION_CHILD_TAG = 'li';
+const AUTH_STATUS_URL           = '/auth-status';
+const LOGIN_URL                 = '/auth-login';
+const LOGOUT_URL                = '/auth-logout';
+const GOOGLE_EMAIL_DOMAIN       = '@google.com';
+const HIDDEN_ATTRIBUTE          = 'hidden';
+const HREF_ATTRIBUTE            = 'href';
+const LOGIN_LOGOUT_BUTTON_ID    = 'login-logout-button';
+const LOGIN_MESSAGE             = 'Login';
+const LOGOUT_MESSAGE            = 'Logout';
+const GOOGLER_ELEMENT_IDS       = ['comment-controls'];
+const NON_GOOGLER_ELEMENT_IDS   = ['comment-no-access-warning'];
 
 /**
-* Opens url in new tab.
-**/
+ * Functions to run when page first loads.
+ **/
+window.onload = function() {
+  adjustDisplayToStatus();
+  loadLoginLogoutButton();
+  loadComments();
+};
+
+
+/**
+ * Loads login / logout button according to the user status.
+ * The button will redirect back to the page where the user click the button.
+ * Clears previously loaded comments when called multiple times (done by setting innerHTML = "").
+ * 'redirect' magic string is intentionally left as is.
+ * This is because constants are taken literally when making objects.
+ **/
+async function loadLoginLogoutButton() {
+  const button      = document.getElementById(LOGIN_LOGOUT_BUTTON_ID);
+  const parameters  = {'redirect': window.location.pathname};
+
+  if (await userIsLoggedIn()) {
+    const logoutFetchUrl    = constructFetchQueryUrl(LOGOUT_URL, parameters);
+    const logoutUrlJson     = await fetch(logoutFetchUrl);
+    const logoutUrl         = await logoutUrlJson.json();
+    button.setAttribute(HREF_ATTRIBUTE, logoutUrl);
+    button.innerText        = LOGOUT_MESSAGE;
+  } else {
+    const loginFetchUrl     = constructFetchQueryUrl(LOGIN_URL, parameters);
+    const loginUrlJson      = await fetch(loginFetchUrl);
+    const loginUrl          = await loginUrlJson.json();
+    button.setAttribute(HREF_ATTRIBUTE, loginUrl);
+    button.innerText        = LOGIN_MESSAGE;
+  }
+}
+
+/**
+ * Adjust the elements that the user will see according to their status.
+ * To add more elements, simply include it in the GOOGLER_ELEMENT_IDS or NON_GOOGLER_ELEMENT_IDS.
+ **/
+async function adjustDisplayToStatus() {
+  if (await userIsGoogler()) {
+    for (id of GOOGLER_ELEMENT_IDS) {
+      let element = document.getElementById(id);
+      element.removeAttribute(HIDDEN_ATTRIBUTE);
+    }
+  } else {
+    for (id of NON_GOOGLER_ELEMENT_IDS) {
+      let element = document.getElementById(id);
+      element.removeAttribute(HIDDEN_ATTRIBUTE);
+    }
+  }
+}
+
+async function userIsGoogler() {
+  return (await userIsLoggedIn()) && (await getUserEmail()).includes(GOOGLE_EMAIL_DOMAIN);
+}
+
+async function userIsLoggedIn() {
+  return await getUserEmail() !== null;
+}
+
+/**
+ * Gets user email. Returns null if not logged in. 
+ **/
+async function getUserEmail() {
+  const emailJson = await fetch(AUTH_STATUS_URL);
+  const email = await emailJson.json();
+  return email;
+}
+
+/**
+ * Opens url in new tab.
+ **/
 function openNewTab(url) {
-    window.open(url);
+  window.open(url);
 }
 
 /**
-* Opens a YouTube video in a new tab given its id.
-* Uses /embed/ path so the video is opened in full screen.
-**/
+ * Opens a YouTube video in a new tab given its id.
+ * Uses /embed/ path so the video is opened in full screen.
+ **/
 function playVideo(id) {
-    openNewTab(YOUTUBE_EMBED_URL + id);
+  openNewTab(YOUTUBE_EMBED_URL + id);
 }
 
 /**
-* Randomly clicks a link that has the given class name.
-**/
+ * Randomly clicks a link that has the given class name.
+ **/
 function clickRandomLink(className) {
   const links       = document.getElementsByClassName(className);
   const randomLink  = links[Math.floor(Math.random() * links.length)];
@@ -44,10 +127,10 @@ function clickRandomLink(className) {
 }
 
 /**
-* load comments on homepage.
-* The number of comments loaded depends of commentsNumber input.
-* Clears previously loaded comments when called multiple times (done by setting innerHTML = "").
-**/
+ * load comments on homepage.
+ * The number of comments loaded depends of commentsNumber input. Will always load 5 at minimum.
+ * Clears previously loaded comments when called multiple times (done by setting innerHTML = "").
+ **/
 async function loadComments(commentsNumber) {
   // 'comments-number' magic string is intentionally left as is.
   // This is because constants are taken literally when making objects.
@@ -65,7 +148,8 @@ async function loadComments(commentsNumber) {
   for (var i = 0; i < commentsObject.length; ++i) {
     let commentProperties   = commentsObject[i].propertyMap;
     let commentText         = commentProperties[COMMENT_TEXT_ID];
-    let commentElement      = createCommentChild(commentText);
+    let commentLdap         = commentProperties[COMMENT_LDAP_ID];
+    let commentElement      = createCommentChild(commentLdap + ': ' + commentText);
     commentSection.prepend(commentElement);
   }
 }
