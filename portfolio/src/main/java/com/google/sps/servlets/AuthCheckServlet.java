cd60fa0ac441bc657b938c30bd5932a7085a0208
Nicholas Husin <nicholashusin@gmail.com>
2020-06-05 15:33:23 +0000

    Split AuthCheck class into a utility class and a servlet
    - Utility functionalities are now implemented in utils/AuthCheck.java
    - Servlet functionalities are now implemented in servlets/AuthCheckServlet.java
    - Add utils directory to use when adding more utilities classes

--- portfolio/src/main/java/com/google/sps/servlets/AuthCheckServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/AuthCheckServlet.java
@@ -7,26 +7,25 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.sps.utils.AuthCheck;
 import com.google.gson.Gson;
 
 /**
- * Authentication class intended to be used mostly as a utility class.
- * Class is used as a utility class rather than a parent to be inherited to prevent functionality bloating.
- * Provides common functions realted to authentication such login, logout, get email, etc.
- * Also provides a convenient GET request listener for client to check the status of user and get login / logout URL.
+ * Servlet class intended to be used by the client to be able to get their authentication status and login / logout link.
+ * All of these functionalities can be called by the client by sending a request to the GET handler.
+ * AuthCheck utility class is used to implement these functionalities.
  *
  * Note that the GET request listener is purely there for the client to be able to display client page appropriately.
- * Backend functionalities should use the utility functions rather than the GET listener.
+ * Backend functionalities should use the utility class AuthCheck rather than the GET listener.
  * Do not rely on the login status sent by the client. Avoid client side validation and re-validate on the server.
  **/
 @WebServlet(urlPatterns={"/auth-status", "/auth-login", "/auth-logout"})
-public class AuthCheck extends HttpServlet {
+public class AuthCheckServlet extends HttpServlet {
   private static final String AUTH_STATUS_URL              = "/auth-status";
   private static final String AUTH_LOGIN_URL               = "/auth-login";
   private static final String AUTH_LOGOUT_URL              = "/auth-logout";
   private static final String REDIRECT_URL_PARAMETER       = "redirect";
   private static final String RESPONSE_CONTENT_TYPE_JSON   = "application/json;";
-  private static final String GOOGLE_EMAIL_DOMAIN          = "@google.com";
 
   /**
    * Get request handler for client usage providing different usages based on where the request is sent.
@@ -41,58 +40,18 @@ public class AuthCheck extends HttpServlet {
     String responseString = null;
 
     if (requestUrl.equals(AUTH_STATUS_URL)) {
-      responseString = getEmail();
+      responseString = AuthCheck.getEmail();
     }
 
     if (requestUrl.equals(AUTH_LOGIN_URL)) {
-      responseString = generateLoginUrl(redirectUrl);
+      responseString = AuthCheck.generateLoginUrl(redirectUrl);
     } 
 
     if (requestUrl.equals(AUTH_LOGOUT_URL)) {
-      responseString = generateLogoutUrl(redirectUrl);
+      responseString = AuthCheck.generateLogoutUrl(redirectUrl);
     }
 
     response.getWriter().print(stringToJson(responseString));
-  }
-
-  static public boolean isLoggedIn() {
-    UserService userService = UserServiceFactory.getUserService();
-    return userService.isUserLoggedIn();
-  }
-
-  static public String generateLoginUrl(String redirectUrl) {
-    UserService userService = UserServiceFactory.getUserService();
-    return userService.createLoginURL(redirectUrl); 
-  }
-
-  static public String generateLogoutUrl(String redirectUrl) {
-    UserService userService = UserServiceFactory.getUserService();
-    return userService.createLogoutURL(redirectUrl); 
-  }
-
-  static public String getEmail() {
-    if (!isLoggedIn()) {
-      return null;
-    }
-
-    UserService userService = UserServiceFactory.getUserService();
-    return userService.getCurrentUser().getEmail();
-  }
-
-  static public boolean isGoogleEmail() {
-    if (!isLoggedIn()) {
-      return false;
-    }
-
-    return getEmail().contains(GOOGLE_EMAIL_DOMAIN);
-  }
-
-  static public String getLdap() {
-    if (!isGoogleEmail()) {
-      return null;
-    }
-
-    return getEmail().replace(GOOGLE_EMAIL_DOMAIN, "");
   }
 
   private String stringToJson(String str) {
